4c633de4afcca7d45c677945ac9570e504bad9c2
Kyou12138 <1129057944@qq.com>
2024-03-11 21:24:43 +0800

    <add>leetcode 206.反转链表

---
+++ Kyou.Practice.Algorithm/ReverseLinkedList.cpp
@@ -0,0 +1,4 @@
+#include "ReverseLinkedList.h"
+
+
+
---
+++ Kyou.Practice.Algorithm/ReverseLinkedList.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+//Definition for singly-linked list.
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class ReverseLinkedList
+{
+
+public:
+	void traverse(ListNode* head) {
+		ListNode* tmp = head;
+		while (tmp) {
+			cout << tmp->val << endl;
+			tmp = tmp->next;
+		}
+	}
+
+	ListNode* reverseList(ListNode* head) {
+		ListNode* pre = nullptr;
+		ListNode* cur = head;
+		while (cur) {
+			ListNode* next = cur->next;
+			cur->next = pre;
+			pre = cur;
+			cur = next;
+		}
+		return pre;
+	}
+
+	void run() {
+
+		ListNode* l4 = new ListNode(4);
+		ListNode* l3 = new ListNode(3, l4);
+		ListNode* l2 = new ListNode(2, l3);
+		ListNode* l1 = new ListNode(1, l2);
+		ListNode* head = new ListNode(0, l1);
+
+		traverse(head);
+
+		ListNode* res = reverseList(head);
+
+		traverse(res);
+
+	}
+};
+
