654767118e21784fa1b0b29b4c1fc5788ee7e1b3
Kyou12138 <1129057944@qq.com>
2024-03-13 22:00:40 +0800

    <add>509.斐波那契数(迭代)

--- Kyou.Practice.Algorithm/Fibonacci.h
+++ Kyou.Practice.Algorithm/Fibonacci.h
@@ -5,9 +5,15 @@ class Fibonacci
 {
 public:
 	void run() {
-		cout << "509. ì³²¨ÄÇÆõÊý" << endl;
+		cout << "509. ì³²¨ÄÇÆõÊý µÝ¹é" << endl;
+
 		int res = fib(10);
 		cout << res << endl;
+
+		cout << "509. ì³²¨ÄÇÆõÊý µü´ú" << endl;
+		res = fib_iteration(10);
+		cout << res << endl;
+
 		cout << endl;
 	}
 	//0 1 1 2 3 5
@@ -15,6 +21,21 @@ public:
 		if (n <= 0) return 0;
 		if (n == 1) return 1;
 		return fib(n - 1) + fib(n - 2);
+	}
+
+	// 0 1 1 2 3 5 8
+	int fib_iteration(int n) {
+		if (n <= 0) return 0;
+		if (n == 1) return 1;
+		int a = 0;
+		int b = 1;
+		while (n > 1) {
+			int tmp = b;
+			b = a + b;
+			a = tmp;
+			n--;
+		}
+		return b;
 	}
 };
 
